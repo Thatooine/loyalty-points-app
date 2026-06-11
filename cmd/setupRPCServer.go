0818722e0249745725b9aecac718185e580d93be
ThatoMaake <tmthatoo4@gmail.com>
2026-06-12 00:17:58 +0200

    Set up JSON-RPC server with Gorilla `mux` and `rpc`, and define base service interface

--- cmd/setupRPCServer.go
+++ cmd/setupRPCServer.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc/v2"
+	gorillaJSON "github.com/gorilla/rpc/v2/json2"
+	"github.com/rs/zerolog/log"
+
+	"github.com/Thatooine/loyalty-points-app/pkg/jsonrpc"
+)
 
 func setupRPCServer(providers ServiceProviders) {
-	fmt.Println("setting up rpc server")
+	port := 8080
+
+	router := mux.NewRouter()
+
+	// create a new json rpc server and register the JSON codec
+	jsonRPCServer := rpc.NewServer()
+	jsonRPCServer.RegisterCodec(gorillaJSON.NewCodec(), "application/json")
+
+	// json rpc services exposed on the /api path
+	services := []jsonrpc.Service{
+		// register services here, e.g.:
+		// loyalty.NewPointsServiceJSONRPCAdaptor(providers.PointsService),
+	}
+
+	// register each service with the json rpc server
+	for _, service := range services {
+		log.Info().Msg("\tRegistering: " + service.Name())
+		if err := jsonRPCServer.RegisterService(service, service.Name()); err != nil {
+			log.Fatal().Err(err).Msg("could not register: " + service.Name())
+		}
+	}
+
+	// create a sub-router at /api and mount the json rpc server on it
+	apiRouter := router.PathPrefix("/api").Subrouter()
+	apiRouter.Use(
+	// add middleware here, e.g. CORS, authentication, logging
+	)
+	apiRouter.Handle("", jsonRPCServer)
+
+	// start the http server
+	log.Info().Msg(fmt.Sprintf("Starting JSON-RPC server on: 0.0.0.0:%d", port))
+	go func() {
+		server := &http.Server{
+			Handler:      router,
+			Addr:         fmt.Sprintf("0.0.0.0:%d", port),
+			WriteTimeout: 150 * time.Second,
+			ReadTimeout:  150 * time.Second,
+		}
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal().Err(err).Msg("error starting json rpc server")
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/Thatooine/loyalty-points-app
 go 1.25.0
 
 require (
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/rpc v1.2.1
 	github.com/rs/zerolog v1.35.1
 	github.com/spf13/viper v1.21.0
 )
